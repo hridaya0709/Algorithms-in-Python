5f91003da3842c573f850abd4fb043d128541ec8
HridayaK <55268471+hridaya0709@users.noreply.github.com>
2020-09-05 18:51:42 +0530

    Explaining some variables and conditions by adding comments

--- Merge_sort.py
+++ Merge_sort.py
@@ -2,8 +2,8 @@ def merge(a,p,q,r):
     l = a[p:q+1]
     r = a[q+1:r+1]
     i,j = 0,0
-    si = p
-    while i < len(l) and j < len(r):
+    si = p #si is starting index
+    while i < len(l) and j < len(r): # Sorting the list by comparing elements in two sub lists a[p:q] & a[q+1:r]
         if l[i] <= r[j]:
             a[si] = l[i]
             i += 1
@@ -11,17 +11,17 @@ def merge(a,p,q,r):
             a[si] = r[j]
             j += 1
         si += 1
-    while i < len(l):
+    while i < len(l): # Checking whether any elements are left in the left list to be sorted
         a[si] = l[i]
         i += 1
         si += 1
-    while j < len(r):
+    while j < len(r): # Checking whether any elements are left in right list to be sorted
         a[si] = r[j]
         j += 1
         si += 1
 def merge_sort(a,p,r):
     if p < r:
-        q = int((p+r)/2)
+        q = int((p+r)/2) #finding the mid point in the list
         merge_sort(a,p,q)
         merge_sort(a,q+1,r)
         merge(a,p,q,r)
